8c6ce076a59744e5f12a427acd0f929be9df4c43
DorotaHeczko <117163329+DorotaHeczko@users.noreply.github.com>
2023-06-10 15:02:17 +0000

    Deploying to gh-pages from @ DorotaHeczko/goit-js-hw-09@a98ba57bd1f90237c5339386bfee8cc48e1db343 🚀

---
+++ 01-color-switcher.e3bcbfeb.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");function d(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,intervalId=setInterval(d,1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(intervalId)}));
+//# sourceMappingURL=01-color-switcher.e3bcbfeb.js.map
